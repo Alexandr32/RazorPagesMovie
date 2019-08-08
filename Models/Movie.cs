021dfa658691eb31e362c551d34518ef826dc85e
Alexandr <alex3289@gmail.com>
2019-08-08 13:40:24 +0600

    Добавлен код приложения

---
+++ Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Название"),
+        StringLength(60, MinimumLength = 3),
+        Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Дата выпуска"),
+        DataType(DataType.Date), // Атрибут DataType указывает тип данных (Date). С этим атрибутом: пользователю не требуется вводить сведения о времени в поле даты.  Отображается только дата, а не время.
+        DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] // Параметр ApplyFormatInEditMode указывает, что формат должен применяться при отображении значения для редактирования
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Жанр"),
+        RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"),
+        Required,
+        StringLength(30)]
+        public string Genre { get; set; }
+
+        [Display(Name = "Цена"),
+        Range(1, 100),
+        DataType(DataType.Currency),
+        Column(TypeName = "decimal(18, 2)")] // позволяет Entity Framework Core корректно сопоставить Price с валютой в базе данных.
+        public decimal Price { get; set; }
+
+        [Display(Name = "Рейтинг"),
+        RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"),
+        StringLength(5),
+        Required]
+        public string Rating { get; set; }
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Строка для поиска
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        /// <summary>
+        /// Содержит список жанров.
+        /// </summary>
+        public SelectList Genres { get; set; }
+
+        /// <summary>
+        /// Содержит конкретный жанр, выбранный пользователем,
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            // Запрос LINQ, который извлекает все жанры из базы данных.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                // Выборка по условию поиску
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                // Выборка по условию выбранного жанра
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            // Список жанров SelectList создается путем проецирования отдельных жанров.
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+
+
+            // Запросы LINQ не выполняются, если они определяются или изменяются
+            // путем вызова метода(например, Where, Contains или OrderBy).Вместо этого
+            // выполнение запроса откладывается. Это означает, что вычисление выражения
+            // откладывается до тех пор, пока не будет выполнена итерация его реализованного
+            // значения или не будет вызван метод ToListAsync.
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
